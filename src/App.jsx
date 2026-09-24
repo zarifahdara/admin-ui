e85e87c985cc320319e46851a723285936afc3e9
zarifahdara <zarifahdaraningtias@gmail.com>
2026-09-24 14:17:54 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="bg-blue-200">Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h3>Zarifah Dara Ningtias</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
